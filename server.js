b3b4039d03e16abc47683f4f9a5f21e259b0a9cf
KKSawarn <35791869+KKSawarn@users.noreply.github.com>
2019-12-24 15:00:53 +0530

    comment line code removed from server.js

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 var fs = require('fs');
-//bootup time load data
 var jsonContent = fs.readFileSync('./data.json', 'utf-8');
 var jsonDataPut = null;
 app.get('/', (req, res) => {
-    // json file data fatching
     var jsonDataGet = jsonContent;
     console.log(jsonDataGet);
     res.send(jsonDataGet);
