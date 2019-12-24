01045cf84e3093ee63d07d8c1b11e71ff3a1697a
KKSawarn <35791869+KKSawarn@users.noreply.github.com>
2019-12-24 15:34:45 +0530

    variable jsonDataget removed. Only jsonContent variable is sufficient.

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const app = express();
 const port = 8080;
 var fs = require('fs');
 var jsonContent = fs.readFileSync('./data.json', 'utf-8');
-var jsonDataPut = null;
 app.get('/', (req, res) => {
-    var jsonDataGet = jsonContent;
-    console.log(jsonDataGet);
-    res.send(jsonDataGet);
+    console.log(jsonContent);
+    res.send(jsonContent);
 })
 app.put('/', (req, res) => {
     jsonDataPut = fs.readFileSync('./data.json','utf-8');
